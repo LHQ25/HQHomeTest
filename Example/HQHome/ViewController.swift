6025d26f9748a0eb94b2a9205a40fb89e993ee0c
louhanqing <louhanqing2012@163.com>
2023-06-04 17:42:04 +0800

    Initial commit

---
+++ Example/HQHome/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HQHome
+//
+//  Created by louhanqing on 06/04/2023.
+//  Copyright (c) 2023 louhanqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
